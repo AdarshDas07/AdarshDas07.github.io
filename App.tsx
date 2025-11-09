efe250a1e7fa23f656b28f8313a1dd0742d44e39
Adarsh Das <165044669+AdarshDas07@users.noreply.github.com>
2025-11-09 17:44:45 -0600

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,11 +1,9 @@
-
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import HomePage from './components/HomePage';
 import ProjectsPage from './components/ProjectsPage';
 import AwardsPage from './components/AwardsPage';
-import ExtracurricularsPage from './components/ExtracurricularsPage';
 import ResearchComponent from './components/ResearchPage';
 
 const App: React.FC = () => {
@@ -17,7 +15,6 @@ const App: React.FC = () => {
           <Route path="projects" element={<ProjectsPage />} />
           <Route path="research" element={<ResearchComponent />} />
           <Route path="awards" element={<AwardsPage />} />
-          <Route path="extracurriculars" element={<ExtracurricularsPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
